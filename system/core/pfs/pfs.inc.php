d42982059604211d468ce621b85dc7abea3ad9c6
Seditio Team <amro86@ya.ru>
2014-11-18 11:20:52 +0300

    Fix width input ntitle

--- system/core/pfs/pfs.inc.php
+++ system/core/pfs/pfs.inc.php
@@ -556,7 +556,7 @@ $disp_upload .= "<td class=\"coltop\">".$L['File']."</td></tr>";
 for ($ii = 0; $ii < $cfg['pfsmaxuploads']; $ii++)
 	{
 	$disp_upload .= "<tr><td style=\"text-align:center;\">#".($ii+1)."</td>\n";
-	$disp_upload .= "<td style=\"width:48%;\"><input type=\"text\" class=\"text\" name=\"ntitle[$ii]\" value=\"\" size=\"48\" maxlength=\"255\" /></td>\n";
+	$disp_upload .= "<td style=\"width:48%;\"><input type=\"text\" class=\"text\" name=\"ntitle[$ii]\" value=\"\" size=\"30\" maxlength=\"255\" /></td>\n";
 	$disp_upload .= "<td style=\"width:48%;\"><input name=\"userfile[$ii]\" type=\"file\" class=\"file\" size=\"32\" />\n";
   $disp_upload .= ($ii+1==$cfg['pfsmaxuploads']) ? " &nbsp; <a href=\"javascript:sedjs.toggleblock('moreuploads')\"><img src=\"skins/".$skin."/img/system/arrow-down.gif\" alt=\"\" /></a>": '';
   $disp_upload .= "</td></tr>";
