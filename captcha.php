2818d18a1897459378dbcba7b37e832fdf1f9b40
Seditio Team <amro86@ya.ru>
2021-02-19 20:22:24 +0400

    Add Captcha & Resize & Crom functions

---
+++ captcha.php
@@ -0,0 +1,15 @@
+<?PHP
+
+define('SED_CODE', TRUE);
+
+require('system/functions.php');
+require('datas/config.php');
+require('system/common.php');
+
+$cfg['font_dir'] = "datas/fonts/";
+
+$captcha_code = sed_generate_code();
+
+sed_captcha_image($captcha_code);
+
+?>
