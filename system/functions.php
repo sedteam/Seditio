1053fe4d42b9c3bcc5323b8dcf8b18ced3a43c45
Seditio Team <amro86@ya.ru>
2022-07-02 22:17:03 +0400

    Working with code comments. Code cleanup.

--- system/functions.php
+++ system/functions.php
@@ -1716,6 +1716,66 @@ function sed_cache_store($name, $value, $expire, $auto = 1)
 	$sql = sed_sql_query("REPLACE INTO $db_cache (c_name, c_value, c_expire, c_auto) VALUES ('$name', '".sed_sql_prep(serialize($value))."', '".($expire + $sys['now'])."', '$auto')");
 	return(TRUE);
 	}
+	
+/** 
+ * Build & display captcha image
+ * 
+ * @param string $code Captcha code
+ * @return mixed 
+ */ 		
+function sed_captcha_image($code)
+    {
+        global $cfg;
+		
+		$image = imagecreatetruecolor(150, 70);
+        imagesetthickness($image, 2);
+
+        $background_color = imagecolorallocate($image, rand(220, 255), rand(220, 255), rand(220, 255));
+        imagefill($image, 0, 0, $background_color);
+
+        $linenum = rand(3, 5);
+        for ($i = 0; $i < $linenum; $i++)
+        {
+            $color = imagecolorallocate($image, rand(0, 150), rand(0, 100), rand(0, 150));
+            imageline($image, rand(0, 150), rand(1, 70), rand(20, 150), rand(1, 70), $color);
+        }
+
+        $font_arr = array_values(array_diff(scandir(SED_ROOT . "/". $cfg['font_dir']), array('.', '..')));
+        $font_size = rand(20, 30);
+        $x = rand(0, 10);
+
+        for ($i = 0; $i < strlen($code); $i++)
+        {
+            $x += 20;
+            $letter = substr($code, $i, 1);
+            $color = imagecolorallocate($image, rand(0, 200), 0, rand(0, 200));
+            $current_font = rand(0, sizeof($font_arr) - 1);
+
+            imagettftext($image, $font_size, rand(-10, 10), $x, rand(50, 55), $color, SED_ROOT . "/". $cfg['font_dir'] . $font_arr[$current_font], $letter);
+        }
+
+        $pixels = rand(2000, 4000);
+        for ($i = 0; $i < $pixels; $i++)
+        {
+            $color = imagecolorallocate($image, rand(0, 200), rand(0, 200), rand(0, 200));
+            imagesetpixel($image, rand(0, 150), rand(0, 150), $color);
+        }		
+		
+        for ($i = 0; $i < $linenum; $i++)
+        {
+            $color = imagecolorallocate($image, rand(0, 255), rand(0, 200), rand(0, 255));
+            imageline($image, rand(0, 20), rand(1, 50), rand(150, 180), rand(1, 50), $color);
+        }
+		
+        header("Expires: Mon, 26 Jul 1997 05:00:00 GMT");
+        header("Last-Modified: " . gmdate("D, d M Y H:i:s", 10000) . " GMT");
+        header("Cache-Control: no-store, no-cache, must-revalidate");
+        header("Cache-Control: post-check=0, pre-check=0", false);
+        header("Pragma: no-cache");
+        header("Content-type: image/png");
+        imagepng($image);
+        imagedestroy($image);
+    }
 
 /** 
  * Makes HTML sequences safe 
@@ -2317,6 +2377,18 @@ function sed_diemaintenance()
 	  exit;
 	  } 
 	} 
+	
+/** 
+ * Counter captcha error message  
+ * 
+ * @param string $message Error message
+ * @return string 
+ */ 
+function sed_error_msg($message)
+    {
+		$_SESSION[$_SERVER['REMOTE_ADDR']]++;
+        return($message);
+    }
 
 /** 
  * Gets details for forum section 
@@ -2470,6 +2542,45 @@ function sed_gen_letteravatar($text, $uid, $fontSize, $imgWidth, $imgHeight)
 			return array('status' => FALSE);	
 		}	
 	}
+	
+/** 
+ * Generate captcha code
+ * 
+ * @return string 
+ */ 	
+function sed_generate_code()
+    {
+        $chars = 'abcdefghijklmnopqrstuvwxyz0123456789';
+        $length = rand(4, 6);
+        $numChars = strlen($chars);
+
+        $str = '';
+        for ($i = 0; $i < $length; $i++)
+        {
+            $str .= substr($chars, rand(1, $numChars) - 1, 1);
+        }
+
+        $array_mix = preg_split('//', $str, -1, PREG_SPLIT_NO_EMPTY);
+        srand((float) microtime() * 1000000);
+        shuffle($array_mix);
+
+        $result = implode("", $array_mix);
+        sed_session_write($result);
+
+        return $result;
+    }
+	
+/** 
+ * Generate field code
+ * 
+ * @return string 
+ */ 	
+function sed_generate_field_code()
+    {
+        $captcha_field = md5(md5(uniqid('', true) . date('His')));
+        sed_session_field_write($captcha_field);
+        return $captcha_field;
+    }
 
 /** 
  * Returns a list of plugins registered for a hook 
@@ -3202,6 +3313,43 @@ function sed_is_ssl()   // New in 175
 		} 
 	return false; 
   } 
+
+/** 
+ * Check user agent is bot? 
+ * 
+ * @return bool
+ */   
+function sed_is_bot()
+	{
+	if (!empty($_SERVER['HTTP_USER_AGENT'])) {
+		$options = array(
+			'YandexBot', 'YandexAccessibilityBot', 'YandexMobileBot','YandexDirectDyn',
+			'YandexScreenshotBot', 'YandexImages', 'YandexVideo', 'YandexVideoParser',
+			'YandexMedia', 'YandexBlogs', 'YandexFavicons', 'YandexWebmaster',
+			'YandexPagechecker', 'YandexImageResizer','YandexAdNet', 'YandexDirect',
+			'YaDirectFetcher', 'YandexCalendar', 'YandexSitelinks', 'YandexMetrika',
+			'YandexNews', 'YandexNewslinks', 'YandexCatalog', 'YandexAntivirus',
+			'YandexMarket', 'YandexVertis', 'YandexForDomain', 'YandexSpravBot',
+			'YandexSearchShop', 'YandexMedianaBot', 'YandexOntoDB', 'YandexOntoDBAPI',
+			'Googlebot', 'Googlebot-Image', 'Mediapartners-Google', 'AdsBot-Google',
+			'Mail.RU_Bot', 'bingbot', 'Accoona', 'ia_archiver', 'Ask Jeeves', 
+			'OmniExplorer_Bot', 'W3C_Validator', 'WebAlta', 'YahooFeedSeeker', 'Yahoo!',
+			'Ezooms', '', 'Tourlentabot', 'MJ12bot', 'AhrefsBot', 'SearchBot', 'SiteStatus', 
+			'Nigma.ru', 'Baiduspider', 'Statsbot', 'SISTRIX', 'AcoonBot', 'findlinks', 
+			'proximic', 'OpenindexSpider','statdom.ru', 'Exabot', 'Spider', 'SeznamBot', 
+			'oBot', 'C-T bot', 'Updownerbot', 'Snoopy', 'heritrix', 'Yeti',
+			'DomainVader', 'DCPbot', 'PaperLiBot'
+		);
+
+		foreach($options as $row) {
+			if (stripos($_SERVER['HTTP_USER_AGENT'], $row) !== false) {
+				return true;
+			}
+		}
+	}
+ 
+	return false;
+	}
 
 /** 
  * Outputs standard javascript 
@@ -4224,6 +4372,21 @@ function sed_selectbox_skin($check, $name)
 
 	return($result);
 	}
+	
+/** 
+ * Return translate & beautify date
+ * 
+ * @param int $timestampDate Timestamp date
+ * @return string 
+ */ 	
+function sed_translate_date($timestampDate)
+	{	
+		global $sed_months_list;		
+		$tday = date("d", $timestampDate);
+		$tmonths = $sed_months_list[(date('n', $timestampDate))];
+		$tyear = date("Y", $timestampDate);		
+		return $tday." ".$tmonths." ".$tyear;
+	}
 
 /** 
  * Returns skin selection radiobox 
@@ -4800,6 +4963,39 @@ function sed_vardump($v, $mode = '')
 		$res = "<pre style=\"white-space:pre-wrap; word-wrap: break-word;\">".htmlspecialchars(ob_get_clean(), ENT_QUOTES)."</pre>";
 		return $res;
 	}
+	
+/** 
+ * Verify captcha code
+ *   
+ * @return string 
+ */ 	
+function sed_verify_code()
+    {		
+		global $L;
+		
+		$captcha_value = $_SESSION['captcha_value'];
+        $captcha_field = $_SESSION['captcha_field'];
+        $answer_time = $_SESSION['answer_time'];
+
+        if (isset($_SESSION[$_SERVER['REMOTE_ADDR']]) && $_SESSION[$_SERVER['REMOTE_ADDR']] >= 10)
+            return sed_error_msg($L['captcha_error_many_incorrect']);
+
+        if (!empty($captcha_value) && !empty($captcha_field) && !empty($answer_time))
+        {
+            $current_time = strtotime(date('d-m-Y H:i:s'));
+
+            if ($current_time - $answer_time < 6)
+                return sed_error_msg($L['captcha_error_you_robot_or_too_fast']);
+            if ($_POST[$captcha_field] == '')
+                return sed_error_msg($L['captcha_error_go_bad_robot']);
+
+            if (md5(md5($_POST[$captcha_field])) == $captcha_value)
+			{ $ok = 1; }
+            else
+			 { return sed_error_msg($L['captcha_error_incorrect']); }
+        }
+        else return sed_error_msg($L['captcha_error_hacker_go_home']);
+    }
   
 /** 
  * Transforms parameters into URL by following user-defined rules into $sed_urltran
@@ -4930,6 +5126,25 @@ function sed_sefurlredirect()
       }   
     }
 }
+
+/** 
+ * Captcha value & answer time write to $_SESSION
+ *   
+ */
+function sed_session_write($code)
+    {      
+		$_SESSION['captcha_value'] = md5(md5($code));
+        $_SESSION['answer_time'] = strtotime(date('d-m-Y H:i:s'));
+    }
+	
+/** 
+ * Captcha field name write to $_SESSION
+ *   
+ */	
+function sed_session_field_write($code)
+    {
+        $_SESSION['captcha_field'] = $code;
+    }
 
 /** 
  * Replace all spaces on separator
@@ -5641,176 +5856,6 @@ function sed_download_img($source_file, $dst_dir, $uid)
     return $dst_file;       
 }
 
-function sed_is_bot()
-	{
-	if (!empty($_SERVER['HTTP_USER_AGENT'])) {
-		$options = array(
-			'YandexBot', 'YandexAccessibilityBot', 'YandexMobileBot','YandexDirectDyn',
-			'YandexScreenshotBot', 'YandexImages', 'YandexVideo', 'YandexVideoParser',
-			'YandexMedia', 'YandexBlogs', 'YandexFavicons', 'YandexWebmaster',
-			'YandexPagechecker', 'YandexImageResizer','YandexAdNet', 'YandexDirect',
-			'YaDirectFetcher', 'YandexCalendar', 'YandexSitelinks', 'YandexMetrika',
-			'YandexNews', 'YandexNewslinks', 'YandexCatalog', 'YandexAntivirus',
-			'YandexMarket', 'YandexVertis', 'YandexForDomain', 'YandexSpravBot',
-			'YandexSearchShop', 'YandexMedianaBot', 'YandexOntoDB', 'YandexOntoDBAPI',
-			'Googlebot', 'Googlebot-Image', 'Mediapartners-Google', 'AdsBot-Google',
-			'Mail.RU_Bot', 'bingbot', 'Accoona', 'ia_archiver', 'Ask Jeeves', 
-			'OmniExplorer_Bot', 'W3C_Validator', 'WebAlta', 'YahooFeedSeeker', 'Yahoo!',
-			'Ezooms', '', 'Tourlentabot', 'MJ12bot', 'AhrefsBot', 'SearchBot', 'SiteStatus', 
-			'Nigma.ru', 'Baiduspider', 'Statsbot', 'SISTRIX', 'AcoonBot', 'findlinks', 
-			'proximic', 'OpenindexSpider','statdom.ru', 'Exabot', 'Spider', 'SeznamBot', 
-			'oBot', 'C-T bot', 'Updownerbot', 'Snoopy', 'heritrix', 'Yeti',
-			'DomainVader', 'DCPbot', 'PaperLiBot'
-		);
 
-		foreach($options as $row) {
-			if (stripos($_SERVER['HTTP_USER_AGENT'], $row) !== false) {
-				return true;
-			}
-		}
-	}
- 
-	return false;
-	}
-
-function sed_captcha_image($code)
-    {
-        global $cfg;
-		
-		$image = imagecreatetruecolor(150, 70);
-        imagesetthickness($image, 2);
-
-        $background_color = imagecolorallocate($image, rand(220, 255), rand(220, 255), rand(220, 255));
-        imagefill($image, 0, 0, $background_color);
-
-        $linenum = rand(3, 5);
-        for ($i = 0; $i < $linenum; $i++)
-        {
-            $color = imagecolorallocate($image, rand(0, 150), rand(0, 100), rand(0, 150));
-            imageline($image, rand(0, 150), rand(1, 70), rand(20, 150), rand(1, 70), $color);
-        }
-
-        $font_arr = array_values(array_diff(scandir(SED_ROOT . "/". $cfg['font_dir']), array('.', '..')));
-        $font_size = rand(20, 30);
-        $x = rand(0, 10);
-
-        for ($i = 0; $i < strlen($code); $i++)
-        {
-            $x += 20;
-            $letter = substr($code, $i, 1);
-            $color = imagecolorallocate($image, rand(0, 200), 0, rand(0, 200));
-            $current_font = rand(0, sizeof($font_arr) - 1);
-
-            imagettftext($image, $font_size, rand(-10, 10), $x, rand(50, 55), $color, SED_ROOT . "/". $cfg['font_dir'] . $font_arr[$current_font], $letter);
-        }
-
-        $pixels = rand(2000, 4000);
-        for ($i = 0; $i < $pixels; $i++)
-        {
-            $color = imagecolorallocate($image, rand(0, 200), rand(0, 200), rand(0, 200));
-            imagesetpixel($image, rand(0, 150), rand(0, 150), $color);
-        }		
-		
-        for ($i = 0; $i < $linenum; $i++)
-        {
-            $color = imagecolorallocate($image, rand(0, 255), rand(0, 200), rand(0, 255));
-            imageline($image, rand(0, 20), rand(1, 50), rand(150, 180), rand(1, 50), $color);
-        }
-		
-        header("Expires: Mon, 26 Jul 1997 05:00:00 GMT");
-        header("Last-Modified: " . gmdate("D, d M Y H:i:s", 10000) . " GMT");
-        header("Cache-Control: no-store, no-cache, must-revalidate");
-        header("Cache-Control: post-check=0, pre-check=0", false);
-        header("Pragma: no-cache");
-        header("Content-type: image/png");
-        imagepng($image);
-        imagedestroy($image);
-    }
-	
-	
-function sed_session_write($code)
-    {      
-		$_SESSION['captcha_value'] = md5(md5($code));
-        $_SESSION['answer_time'] = strtotime(date('d-m-Y H:i:s'));
-    }
-	
-	
-	
-function sed_generate_code()
-    {
-        $chars = 'abcdefghijklmnopqrstuvwxyz0123456789';
-        $length = rand(4, 6);
-        $numChars = strlen($chars);
-
-        $str = '';
-        for ($i = 0; $i < $length; $i++)
-        {
-            $str .= substr($chars, rand(1, $numChars) - 1, 1);
-        }
-
-        $array_mix = preg_split('//', $str, -1, PREG_SPLIT_NO_EMPTY);
-        srand((float) microtime() * 1000000);
-        shuffle($array_mix);
-
-        $result = implode("", $array_mix);
-        sed_session_write($result);
-
-        return $result;
-    }
-	
-function sed_verify_code()
-    {		
-		global $L;
-		
-		$captcha_value = $_SESSION['captcha_value'];
-        $captcha_field = $_SESSION['captcha_field'];
-        $answer_time = $_SESSION['answer_time'];
-
-        if (isset($_SESSION[$_SERVER['REMOTE_ADDR']]) && $_SESSION[$_SERVER['REMOTE_ADDR']] >= 10)
-            return sed_error_msg($L['captcha_error_many_incorrect']);
-
-        if (!empty($captcha_value) && !empty($captcha_field) && !empty($answer_time))
-        {
-            $current_time = strtotime(date('d-m-Y H:i:s'));
-
-            if ($current_time - $answer_time < 6)
-                return sed_error_msg($L['captcha_error_you_robot_or_too_fast']);
-            if ($_POST[$captcha_field] == '')
-                return sed_error_msg($L['captcha_error_go_bad_robot']);
-
-            if (md5(md5($_POST[$captcha_field])) == $captcha_value)
-			{ $ok = 1; }
-            else
-			 { return sed_error_msg($L['captcha_error_incorrect']); }
-        }
-        else return sed_error_msg($L['captcha_error_hacker_go_home']);
-    }
-	
-function sed_error_msg($message)
-    {
-		$_SESSION[$_SERVER['REMOTE_ADDR']]++;
-        return($message);
-    }
-
-function sed_session_field_write($code)
-    {
-        $_SESSION['captcha_field'] = $code;
-    }
-
-function sed_generate_field_code()
-    {
-        $captcha_field = md5(md5(uniqid('', true) . date('His')));
-        sed_session_field_write($captcha_field);
-        return $captcha_field;
-    }
-
-function sed_translate_date($timestampDate)
-	{	
-		global $sed_months_list;		
-		$tday = date("d", $timestampDate);
-		$tmonths = $sed_months_list[(date('n', $timestampDate))];
-		$tyear = date("Y", $timestampDate);		
-		return $tday." ".$tmonths." ".$tyear;
-	}
 
 ?>
