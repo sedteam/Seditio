d9069f553ec10b11c2ebadb63bf34f5bcd3f8417
Seditio Team <amro86@ya.ru>
2025-02-09 16:34:21 +0400

    Modernization Xtemplates, adding support for logical expressions
    <!-- IF {PHP.usr.isadmin} -->
    <p>Welcome back, {PHP.usr.name}!</p>
    <!-- ELSE -->
    <p>Please log in to access this content.</p>
    <!-- ENDIF -->
    
    <!-- IF {PHP.usr.isadmin} && {PHP.usr.id} > 0 -->
    <p>Welcome back, {PHP.usr.name}!</p>
    <!-- ELSE -->
    <p>Please log in to access this content.</p>
    <!-- ENDIF -->

--- system/templates.php
+++ system/templates.php
@@ -25,6 +25,8 @@ Description=Xtemplate class
  * @copyright Barnabas Debreceni 2000-2001
  * @author Jeremy Coates [cocomp@users.sourceforge.net]
  * @copyright Jeremy Coates 2002-2007
+ * @author Tishov Alexander [info@avego.org]
+ * @copyright Tishov Alexander, Seditio Team 2025
  * @see license.txt LGPL / BSD license
  * @since PHP 5
  * @link $HeadURL$
@@ -89,6 +91,44 @@ class XTemplate
 	protected $_error = '';
 	protected $_autoreset = true;
 	protected $_ignore_missing_blocks = true;
+
+	// Syntax for conditions
+	public $if_start_delim = '<!-- IF ';
+	public $if_end_delim = ' -->';
+	public $else_delim = '<!-- ELSE -->';
+	public $endif_delim = '<!-- ENDIF -->';
+
+	// Regular expressions for finding conditions
+	public $if_preg = '`<!-- IF (.*?) -->`';
+	public $else_preg = '`<!-- ELSE -->`';
+	public $endif_preg = '`<!-- ENDIF -->`';
+
+	// Operator precedence for RPN
+	protected $operator_precedence = [
+		'&&' => 2,
+		'||' => 2,
+		'==' => 3,
+		'!=' => 3,
+		'<' => 4,
+		'>' => 4,
+		'<=' => 4,
+		'>=' => 4,
+		'+' => 5,
+		'-' => 5,
+		'*' => 6,
+		'/' => 6,
+		'%' => 6,
+	];
+
+	/**
+	 * Constructor for the XTemplate class.
+	 *
+	 * @param mixed $options Options array or filename.
+	 * @param string $tpldir Template directory.
+	 * @param array $files Array of files.
+	 * @param string $mainblock Main block name.
+	 * @param bool $autosetup Automatically setup the template.
+	 */
 	public function __construct($options, $tpldir = '', $files = null, $mainblock = 'main', $autosetup = true)
 	{
 
@@ -106,6 +146,17 @@ class XTemplate
 		$this->restart($options);
 	}
 
+	/**
+	 * Restart the template engine with new options.
+	 *
+	 * @param mixed $options Options array or filename.
+	 * @param string $tpldir Template directory.
+	 * @param array $files Array of files.
+	 * @param string $mainblock Main block name.
+	 * @param bool $autosetup Automatically setup the template.
+	 * @param string $tag_start Starting delimiter for tags.
+	 * @param string $tag_end Ending delimiter for tags.
+	 */
 	public function restart($options, $tpldir = '', $files = null, $mainblock = 'main', $autosetup = true, $tag_start = '{', $tag_end = '}')
 	{
 
@@ -192,6 +243,11 @@ class XTemplate
 		}
 	}
 
+	/**
+	 * Setup the template engine.
+	 *
+	 * @param bool $add_outer Add outer block.
+	 */
 	public function setup($add_outer = false)
 	{
 
@@ -219,7 +275,13 @@ class XTemplate
 		$this->scan_globals();
 	}
 
-
+	/**
+	 * Assign a variable to the template.
+	 *
+	 * @param mixed $name Variable name or array of variables.
+	 * @param mixed $val Variable value.
+	 * @param bool $reset_array Reset the array.
+	 */
 	public function assign($name, $val = '', $reset_array = true)
 	{
 
@@ -246,6 +308,12 @@ class XTemplate
 		}
 	}
 
+	/**
+	 * Assign a file to the template.
+	 *
+	 * @param mixed $name File variable name or array of file variables.
+	 * @param mixed $val File variable value.
+	 */
 	public function assign_file($name, $val = '')
 	{
 
@@ -261,6 +329,11 @@ class XTemplate
 		}
 	}
 
+	/**
+	 * Parse a block in the template.
+	 *
+	 * @param string $bname Block name.
+	 */
 	public function parse($bname)
 	{
 
@@ -278,6 +351,9 @@ class XTemplate
 
 			$this->_set_error("parse: blockname [$bname] does not exist");
 		}
+
+		// Process conditions
+		$copy = $this->_process_conditions($copy);
 
 		if (!isset($copy)) {
 			die('Block: ' . $bname);
@@ -476,6 +552,11 @@ class XTemplate
 		}
 	}
 
+	/**
+	 * Recursively parse a block in the template.
+	 *
+	 * @param string $bname Block name.
+	 */
 	public function rparse($bname)
 	{
 
@@ -494,6 +575,13 @@ class XTemplate
 		$this->parse($bname);
 	}
 
+	/**
+	 * Insert a loop into the template.
+	 *
+	 * @param string $bname Block name.
+	 * @param string $var Variable name.
+	 * @param mixed $value Variable value.
+	 */
 	public function insert_loop($bname, $var, $value = '')
 	{
 
@@ -501,6 +589,12 @@ class XTemplate
 		$this->parse($bname);
 	}
 
+	/**
+	 * Compress the output by removing whitespace.
+	 *
+	 * @param string $out Output string.
+	 * @return string Compressed output.
+	 */
 	public function compress($out)
 	{
 		$out = preg_replace("/(?:(?:\/\*(?:[^*]|(?:\*+[^*\/]))*\*+\/)|(?:(?<!\:|\\\|\'|\")\/\/.*))/", "", $out);
@@ -508,6 +602,13 @@ class XTemplate
 		return $out;
 	}
 
+	/**
+	 * Loop through an array and insert into the template.
+	 *
+	 * @param string $bname Block name.
+	 * @param string $var Variable name.
+	 * @param array $values Array of values.
+	 */
 	public function array_loop($bname, $var, &$values)
 	{
 
@@ -520,6 +621,12 @@ class XTemplate
 		}
 	}
 
+	/**
+	 * Get the text of a block.
+	 *
+	 * @param string $bname Block name.
+	 * @return string Block text.
+	 */
 	public function text($bname = '')
 	{
 
@@ -554,6 +661,11 @@ class XTemplate
 		return $text;
 	}
 
+	/**
+	 * Output the text of a block.
+	 *
+	 * @param string $bname Block name.
+	 */
 	public function out($bname)
 	{
 		$out = $this->text($bname);
@@ -564,6 +676,12 @@ class XTemplate
 		echo trim($out);
 	}
 
+	/**
+	 * Output the text of a block to a file.
+	 *
+	 * @param string $bname Block name.
+	 * @param string $fname File name.
+	 */
 	public function out_file($bname, $fname)
 	{
 
@@ -575,52 +693,96 @@ class XTemplate
 		}
 	}
 
+	/**
+	 * Reset a block.
+	 *
+	 * @param string $bname Block name.
+	 */
 	public function reset($bname)
 	{
 
 		$this->parsed_blocks[$bname] = '';
 	}
 
+	/**
+	 * Check if a block has been parsed.
+	 *
+	 * @param string $bname Block name.
+	 * @return bool True if parsed, false otherwise.
+	 */
 	public function parsed($bname)
 	{
 
 		return (!empty($this->parsed_blocks[$bname]));
 	}
 
+	/**
+	 * Set the null string for a variable.
+	 *
+	 * @param string $str Null string.
+	 * @param string $varname Variable name.
+	 */
 	public function set_null_string($str, $varname = '')
 	{
 
 		$this->_null_string[$varname] = $str;
 	}
 
+	/**
+	 * Set the null string for a variable (alias).
+	 *
+	 * @param string $str Null string.
+	 * @param string $varname Variable name.
+	 */
 	public function SetNullString($str, $varname = '')
 	{
 		$this->set_null_string($str, $varname);
 	}
 
+	/**
+	 * Set the null block for a block.
+	 *
+	 * @param string $str Null block.
+	 * @param string $bname Block name.
+	 */
 	public function set_null_block($str, $bname = '')
 	{
 
 		$this->_null_block[$bname] = $str;
 	}
 
+	/**
+	 * Set the null block for a block (alias).
+	 *
+	 * @param string $str Null block.
+	 * @param string $bname Block name.
+	 */
 	public function SetNullBlock($str, $bname = '')
 	{
 		$this->set_null_block($str, $bname);
 	}
 
+	/**
+	 * Set auto reset for blocks.
+	 */
 	public function set_autoreset()
 	{
 
 		$this->_autoreset = true;
 	}
 
+	/**
+	 * Clear auto reset for blocks.
+	 */
 	public function clear_autoreset()
 	{
 
 		$this->_autoreset = false;
 	}
 
+	/**
+	 * Scan for global variables.
+	 */
 	public function scan_globals()
 	{
 
@@ -662,6 +824,11 @@ class XTemplate
 		$this->assign('PHP', $GLOB);
 	}
 
+	/**
+	 * Get the error message.
+	 *
+	 * @return string Error message.
+	 */
 	public function get_error()
 	{
 
@@ -684,6 +851,13 @@ class XTemplate
 		return $retval;
 	}
 
+	/**
+	 * Make a tree structure from the template content.
+	 *
+	 * @param string $con Template content.
+	 * @param string $parentblock Parent block name.
+	 * @return array Tree structure.
+	 */
 	public function _maketree($con, $parentblock = '')
 	{
 
@@ -746,6 +920,12 @@ class XTemplate
 		return $blocks;
 	}
 
+	/**
+	 * Assign a file variable to the template.
+	 *
+	 * @param string $name File variable name.
+	 * @param string $val File variable value.
+	 */
 	private function _assign_file_sub($name, $val)
 	{
 
@@ -788,6 +968,12 @@ class XTemplate
 		$this->filevars[$name] = $val;
 	}
 
+	/**
+	 * Store file variable parents.
+	 *
+	 * @param array $blocks Blocks array.
+	 * @return array File variable parents.
+	 */
 	public function _store_filevar_parents($blocks)
 	{
 
@@ -807,12 +993,23 @@ class XTemplate
 		return $parents;
 	}
 
+	/**
+	 * Set an error message.
+	 *
+	 * @param string $str Error message.
+	 */
 	private function _set_error($str)
 	{
 
 		$this->_error .= '* ' . $str . " *\n";
 	}
 
+	/**
+	 * Get the content of a file.
+	 *
+	 * @param string $file File name.
+	 * @return string File content.
+	 */
 	protected function _getfile($file)
 	{
 
@@ -905,6 +1102,12 @@ class XTemplate
 		return $file_text;
 	}
 
+	/**
+	 * Recursively get the content of a file.
+	 *
+	 * @param string $file File name.
+	 * @return string File content.
+	 */
 	public function _r_getfile($file)
 	{
 
@@ -921,6 +1124,242 @@ class XTemplate
 		return $text;
 	}
 
+	/**
+	 * Tokenize an expression.
+	 *
+	 * @param string $expression Expression to tokenize.
+	 * @return array Tokens.
+	 */
+	protected function _tokenize($expression)
+	{
+		// Regular expression for tokenization
+		$pattern = '`\s*(?:(\d*\.\d+|\d+)|([+\-*/%()]|&&|\|\||==|!=|<=|>=|<|>)|([A-Za-z0-9\._\x7f-\xff]+))\s*`';
+		preg_match_all($pattern, $expression, $matches, PREG_SET_ORDER);
+
+		$tokens = [];
+		foreach ($matches as $match) {
+			if (isset($match[1]) && $match[1] !== '') {
+				// Number
+				$tokens[] = ['type' => 'number', 'value' => $match[1]];
+			} elseif (isset($match[2]) && $match[2] !== '') {
+				// Operator or parenthesis
+				$tokens[] = ['type' => 'operator', 'value' => $match[2]];
+			} elseif (isset($match[3]) && $match[3] !== '') {
+				// Variable
+				$tokens[] = ['type' => 'variable', 'value' => $match[3]];
+			}
+		}
+
+		return $tokens;
+	}
+
+	/**
+	 * Convert tokens to Reverse Polish Notation (RPN).
+	 *
+	 * @param array $tokens Tokens.
+	 * @return array RPN.
+	 */
+	protected function _shunting_yard($tokens)
+	{
+		$output = [];
+		$stack = [];
+
+		foreach ($tokens as $token) {
+			if ($token['type'] === 'number' || $token['type'] === 'variable') {
+				// Numbers and variables go directly to output
+				$output[] = $token;
+			} elseif ($token['type'] === 'operator') {
+				// Operators
+				while (
+					!empty($stack) &&
+					$stack[count($stack) - 1]['type'] === 'operator' &&
+					$this->operator_precedence[$stack[count($stack) - 1]['value']] >= $this->operator_precedence[$token['value']]
+				) {
+					$output[] = array_pop($stack);
+				}
+				$stack[] = $token;
+			} elseif ($token['value'] === '(') {
+				// Opening parenthesis to stack
+				$stack[] = $token;
+			} elseif ($token['value'] === ')') {
+				// Closing parenthesis: pop operators to output until opening parenthesis
+				while (!empty($stack) && $stack[count($stack) - 1]['value'] !== '(') {
+					$output[] = array_pop($stack);
+				}
+				array_pop($stack); // Remove opening parenthesis
+			}
+		}
+
+		// Pop remaining operators to output
+		while (!empty($stack)) {
+			$output[] = array_pop($stack);
+		}
+
+		return $output;
+	}
+
+	/**
+	 * Get the value of a variable.
+	 *
+	 * @param string $var_name Variable name.
+	 * @return mixed Variable value.
+	 */
+	protected function _get_variable_value($var_name)
+	{
+		$parts = explode('.', $var_name);
+		$value = $this->vars;
+
+		foreach ($parts as $part) {
+			if (is_array($value) && isset($value[$part])) {
+				$value = $value[$part];
+			} elseif (is_object($value) && isset($value->$part)) {
+				$value = $value->$part;
+			} else {
+				return null;
+			}
+		}
+
+		return $value;
+	}
+
+	/**
+	 * Evaluate Reverse Polish Notation (RPN).
+	 *
+	 * @param array $rpn RPN.
+	 * @return mixed Evaluation result.
+	 */
+	protected function _evaluate_rpn($rpn)
+	{
+		$stack = [];
+
+		foreach ($rpn as $token) {
+			if ($token['type'] === 'number') {
+				// Number: push to stack
+				$stack[] = $token['value'];
+			} elseif ($token['type'] === 'variable') {
+				// Variable: replace with value and push to stack
+				$value = $this->_get_variable_value($token['value']);
+				$stack[] = $value;
+			} elseif ($token['type'] === 'operator') {
+				// Operator: evaluate
+				$b = array_pop($stack);
+				$a = array_pop($stack);
+				$result = $this->_apply_operator($token['value'], $a, $b);
+				$stack[] = $result;
+			}
+		}
+
+		return array_pop($stack);
+	}
+
+	/**
+	 * Evaluate a condition.
+	 *
+	 * @param string $condition Condition to evaluate.
+	 * @return bool Evaluation result.
+	 */
+	protected function _evaluate_condition($condition)
+	{
+		// Tokenize the expression
+		$tokens = $this->_tokenize($condition);
+
+		// Convert to Reverse Polish Notation (RPN)
+		$rpn = $this->_shunting_yard($tokens);
+
+		// Evaluate RPN
+		return (bool) $this->_evaluate_rpn($rpn);
+	}
+
+	/**
+	 * Apply an operator to two values.
+	 *
+	 * @param string $operator Operator.
+	 * @param mixed $a First value.
+	 * @param mixed $b Second value.
+	 * @return mixed Result of the operation.
+	 * @throws Exception If the operator is unknown.
+	 */
+	protected function _apply_operator($operator, $a, $b)
+	{
+		switch ($operator) {
+			case '+':
+				return $a + $b;
+			case '-':
+				return $a - $b;
+			case '*':
+				return $a * $b;
+			case '/':
+				return $a / $b;
+			case '%':
+				return $a % $b;
+			case '==':
+				return $a == $b;
+			case '!=':
+				return $a != $b;
+			case '<':
+				return $a < $b;
+			case '>':
+				return $a > $b;
+			case '<=':
+				return $a <= $b;
+			case '>=':
+				return $a >= $b;
+			case '&&':
+				return $a && $b;
+			case '||':
+				return $a || $b;
+			default:
+				throw new Exception("Unknown operator: $operator");
+		}
+	}
+
+	/**
+	 * Process conditions in the template content.
+	 *
+	 * @param string $content Template content.
+	 * @return string Processed content.
+	 */
+	protected function _process_conditions($content)
+	{
+		// Process all conditions in the template
+		while (preg_match($this->if_preg, $content, $if_matches)) {
+			$condition = trim($if_matches[1]); // // Condition (e.g., "{PHP.i} % 2 == 0 && {PHP.usr.isadmin}")
+			$if_block_start = strpos($content, $if_matches[0]); // Start of IF block
+			$if_block_length = strlen($if_matches[0]); // Length of IF block
+
+			// Find the end of the IF block (either ELSE or ENDIF)
+			$else_pos = strpos($content, $this->else_delim, $if_block_start + $if_block_length);
+			$endif_pos = strpos($content, $this->endif_delim, $if_block_start + $if_block_length);
+
+			// Determine if there is an ELSE
+			$has_else = ($else_pos !== false && $else_pos < $endif_pos);
+
+			// Calculate the boundaries of the blocks
+			$if_content_start = $if_block_start + $if_block_length;
+			$if_content_end = $has_else ? $else_pos : $endif_pos;
+			$else_content_start = $has_else ? $else_pos + strlen($this->else_delim) : null;
+			$else_content_end = $has_else ? $endif_pos : null;
+
+			// Extract the content of the blocks
+			$if_content = substr($content, $if_content_start, $if_content_end - $if_content_start);
+			$else_content = $has_else ? substr($content, $else_content_start, $else_content_end - $else_content_start) : '';
+
+			// Evaluate the condition
+			$condition_result = $this->_evaluate_condition($condition);
+
+			// Replace the IF block with the corresponding content
+			$replacement = $condition_result ? $if_content : $else_content;
+			$content = substr_replace($content, $replacement, $if_block_start, $endif_pos + strlen($this->endif_delim) - $if_block_start);
+		}
+
+		return $content;
+	}
+
+	/**
+	 * Trim callback parameters.
+	 *
+	 * @param string $value Callback parameter.
+	 */
 	protected function trim_callback(&$value)
 	{
 		$value = preg_replace($this->preg_delimiter . "^.*(%s).*$" . $this->preg_delimiter, '\\1', trim($value));
@@ -931,6 +1370,9 @@ class XTemplate
 		$value = preg_replace($this->preg_delimiter . '\\\,' . $this->preg_delimiter, ',', $value);
 	}
 
+	/**
+	 * Add an outer block to the template content.
+	 */
 	private function _add_outer_block()
 	{
 
@@ -940,6 +1382,11 @@ class XTemplate
 		$this->filecontents = $before . "\n" . $this->filecontents . "\n" . $after;
 	}
 
+	/**
+	 * Debug var_dump.
+	 *
+	 * @param mixed $args Arguments to var_dump.
+	 */
 	protected function _pre_var_dump($args)
 	{
 
@@ -950,6 +1397,12 @@ class XTemplate
 		}
 	}
 
+	/**
+	 * Debug var_dump with output buffering.
+	 *
+	 * @param mixed $args Arguments to var_dump.
+	 * @return string Output of var_dump.
+	 */
 	protected function _ob_var_dump($args)
 	{
 
